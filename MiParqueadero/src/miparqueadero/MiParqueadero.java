90e3377afdc348aae033d2843d27043637a600b6
Nestor Sanchez <nestor.sanchez94@gmail.com>
2018-06-01 12:04:58 -0500

    commit 3

--- MiParqueadero/src/miparqueadero/MiParqueadero.java
+++ MiParqueadero/src/miparqueadero/MiParqueadero.java
@@ -5,6 +5,8 @@
  */
 package miparqueadero;
 
+import java.util.*;
+
 /**
  *
  * @author Estudiantes
@@ -15,7 +17,36 @@ public class MiParqueadero {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Vehiculo miVehiculo = new Vehiculo();
+        Scanner miScanner = new Scanner(System.in);
+        int entrada = 0;
+        System.out.println("Ingrese 1 para ingresar su vehiculo");
+        System.out.println("Ingrese 2 para retirar su vehiculo");
+        System.out.println("Ingrese 3 para consultar cupos disponibles");
+
+        if (1 <= entrada  && entrada <= 3) {
+            switch (entrada) {
+                case 1:
+                    System.out.println("Los datos de su vehiculo es");
+                    System.out.println("#Placa");
+                    break;
+                case 2:
+                    System.out.println("El costo del parqueadero es");
+                    System.out.println("Tiempo");
+                    break;
+                case 3:
+                    System.out.println("Los cupos disponibles son");
+                    System.out.println("#");
+                    break;
+                default:
+                     
+                System.out.println("Este valor no es aceptado");
+               
+
+            }
+
+        }
     }
-    
+
 }
