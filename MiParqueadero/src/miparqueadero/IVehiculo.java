10ccc909aaae3391dd033b19e7648edfb8618a94
Nestor Sanchez <nestor.sanchez94@gmail.com>
2018-06-01 10:49:11 -0500

    commit 1

---
+++ MiParqueadero/src/miparqueadero/IVehiculo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miparqueadero;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public interface IVehiculo {
+    
+    private String placa();
+    private String propietario();
+    private float cilindraje();
+}
