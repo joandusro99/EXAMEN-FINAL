dd3e1d0b94a56739589eff31b603b3f473bb3a57
Nestor Sanchez <nestor.sanchez94@gmail.com>
2018-06-01 11:47:35 -0500

    commit 2

---
+++ MiParqueadero/src/miparqueadero/Vehiculo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miparqueadero;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Vehiculo implements IVehiculo {
+
+    private String placa;
+    private String propietario;
+    private float cilindraje;
+
+    @Override
+    public String placa() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String propietario() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float cilindraje() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
